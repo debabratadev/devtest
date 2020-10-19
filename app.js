1bc743b89408bbe5b59d3457a2b3ba240c392f2e
debabratadev <debabratakandev@gmail.com>
2020-10-19 21:30:55 +0530

    Add view for displaying users.

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = require('./src/routes/router')
 
-
-
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello Worlds!')
+    res.render('home', {title: "Great"});
 });
 
 app.use('/api/',router);
