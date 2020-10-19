453d33526aa3501afb86c096128747e816e0eaf4
debabratadev <debabratakandev@gmail.com>
2020-10-19 22:24:43 +0530

    Show user list in home page.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const router = require('./src/routes/router')
+const router = require('./src/routes/router');
+const home = require('./src/services/home');
 
 const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('home', {title: "Great"});
+    (async function() {
+        const result = await home.fetchUsers();
+        
+        console.log(result);
+        res.render('home', {users:result});
+    })();
 });
 
+app.use(express.static(__dirname));
 app.use('/api/',router);
 
 require('./src/db/mysql');
---
+++ src/services/home.js
@@ -0,0 +1,23 @@
+const con = require('../db/mysql');
+
+async function fetchUsers() {
+    try {
+        return new Promise((resolve, reject) => {
+
+            con.query("SELECT * FROM users", function (err, result, fields) {
+                if (err) throw err;
+                resolve(result);
+                return result;
+            });
+        });
+
+        
+    }
+    catch (e) {
+        console.log(e)
+        res.status(400).send({ error: e });
+    }
+
+}
+
+module.exports = { fetchUsers };
