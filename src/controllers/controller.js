f170d8030f64e04753230e6a6a99bd8a2fdc2195
debabratadev <debabratakandev@gmail.com>
2020-10-20 15:14:40 +0530

    Insert dates into table.

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,6 +1,7 @@
 const con = require('../db/mysql');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
+const { PendingXHR } = require('pending-xhr-puppeteer');
 
 const getUsers = async (req, res) => {
     try {
@@ -46,25 +47,81 @@ const fetchUsers = async (page) => {
 
 const fetchDataFromWebsite = async (req, res) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 20 // slow down by 250ms
+        });
         const page = await browser.newPage();
-        await page.goto('https://develop.pub.afflu.net');
+        const pendingXHR = new PendingXHR(page);
 
+        await page.goto('https://develop.pub.afflu.net', {
+            waitUntil: 'networkidle0',
+        });
+
+        // Login
         await page.type('body > div.content > form.login-form > div:nth-child(2) > input', 'developertest@affluent.io');
         await page.type('body > div.content > form.login-form > div:nth-child(3) > input', 'SOpcR^37');
-
         await Promise.all([
             await page.click('body > div.content > form.login-form > div.form-actions > button'),
             page.waitForNavigation({ waitUntil: 'networkidle0' }),
         ]);
 
+        await page.goto('https://develop.pub.afflu.net/list?type=dates&startDate=2020-04-01&endDate=2020-04-30', {
+            waitUntil: 'networkidle0',
+        });
+
         // await page.screenshot({ path: 'example.png' });
+
+        const nextButton = '#DataTables_Table_0_wrapper div.dataTables_paginate > ul.pagination a[title="Next"]';
+        
+        // Fetch from table
+        const records = [];
+        for (let index = 0; index < 6; index++) {
+            if(index !== 0) {
+                console.log(index);
+                await page.click(nextButton);
+                await pendingXHR.waitForAllXhrFinished();
+                await page.waitFor(1000);
+            }
+
+            let chunks = await page.evaluate(() => {
+                const tds = Array.from(document.querySelectorAll('#DataTables_Table_0 tr td'))
+                return tds.map(td => td.innerText)
+            });
+
+            while(chunks.length) records.push(chunks.splice(0,8));
+        }
+        
+        let conRecords = [];
+
+        // Make data insertable into table
+        records.forEach(record => {
+
+            let conRecord = [];
+
+            record.forEach((single) => {
+                conRecord.push(`'${single}'`);
+            });
+
+            conRecords.push(` ( ${conRecord.join(', ')} )`);
+        });
+
+        const values = conRecords.join(', ');
+
+        console.log(values);
+
+        // Insert into table
+        var sql = "INSERT INTO dates (date, commisions, sales, leads, clicks, epc, impressions, cr) VALUES " + values;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("inserted");
+        });
 
         await browser.close();
 
         res.status(200).send({
             status: 200,
-            message: 'Login Success'
+            message: 'Success'
         });
     }
     catch (e) {
